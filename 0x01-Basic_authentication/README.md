bfe0857fd2012fc3b861d54b535fcf670bfe5412
Goddaybc <goddayoghenerobiz@gmail.com>
2023-06-06 19:55:47 -0700

    basic authentication

--- 0x01-Basic_authentication/README.md
+++ 0x01-Basic_authentication/README.md
@@ -1,27 +1,17 @@
-0x01. Basic authentication
-==========================
+# 0x01 Basic authentication
 
-Resources
----------
+## Files
 
-**Read or watch**:
-
--   [REST API Authentication Mechanisms](https://alx-intranet.hbtn.io/rltoken/ssg5umgsMk5jKM8WRHk2Ug "REST API Authentication Mechanisms")
--   [Base64 in Python](https://alx-intranet.hbtn.io/rltoken/RpaPRyKx1rdHgRSUyuPfeg "Base64 in Python")
--   [HTTP header Authorization](https://alx-intranet.hbtn.io/rltoken/WlARq8tQPUGQq5VphLKM4w "HTTP header Authorization")
--   [Flask](https://alx-intranet.hbtn.io/rltoken/HG5WXgSja5kMa29fbMd9Aw "Flask")
--   [Base64 - concept](https://alx-intranet.hbtn.io/rltoken/br6Rp4iMaOce6EAC-JQnOw "Base64 - concept")
-
-## Description
-How to implement basic authentication
-## Tasks
-* [README.md](README.md)
-* [api](api)
-* [main_0.py](main_0.py)
-* [main_1.py](main_1.py)
-* [main_2.py](main_2.py)
-* [main_3.py](main_3.py)
-* [main_4.py](main_4.py)
-* [main_6.py](main_6.py)
-* [models](models)
-* [requirements.txt](requirements.txt)
+| Files                | Description                                  |
+| -------------------- | -------------------------------------------- |
+| **requirements.txt** | Requeriments                                 |
+| **models**           | Models                                       |
+| **main_0.py**        | Auth class                                   |
+| **main_1.py**        | Define which routes don't need authenticatio |
+| **main_2.py**        | Basic - Base64 part                          |
+| **main_3.py**        | Basic - Base64 decode                        |
+| **main_4.py**        | Basic - User credentials                     |
+| **main_5.py**        | Basic - User object                          |
+| **main_6.py**        | Basic - Overload current_user - and BOOM!    |
+| **main_100.py**      | Basic - Allow password with ":"              |
+| **api**              | Flask                                        |
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,50 +1,71 @@
 #!/usr/bin/env python3
+""" Module of auth
 """
-Auth class
-"""
-
-from tabnanny import check
 from flask import request
-from typing import TypeVar, List
-User = TypeVar('User')
+from typing import List, TypeVar
 
 
 class Auth:
-    """
-    a class to manage the API authentication
-    """
+    """ Auth Class """
+
+    def __init__(self):
+        """
+            Constructor
+
+            Args:
+                path: path to authenticate
+                excluded_paths: list of excluded path to authenticate
+        """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        returns False - path and excluded_paths
+            Require the auth
+
+            Args:
+                path: path to authenticate
+                excluded_paths: list of excluded path to authenticate
+
+            Return:
+                True if is authenticated otherwise false
         """
-        check = path
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
-        if path[-1] != "/":
-            check += "/"
-        if check in excluded_paths or path in excluded_paths:
-            return False
-        with_stars = filter(
-            lambda p: p.endswith("*"),
-            excluded_paths
-        )
-        for p in with_stars:
-            strip = p[:-1]
-            if path.startswith(strip):
+
+        if path[-1] is not '/':
+            path += '/'
+
+        for paths in excluded_paths:
+            if paths.endswith('*'):
+                if path.startswith(paths[:-1]):
+                    return False
+            elif path == paths:
                 return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
         """
-        returns None - request
+            Look the headers
+
+            Args:
+                request: Look the autthorization
+
+            Return:
+                The authorization header or None
         """
         if request is None:
             return None
-        return request.headers.get("Authorization")
 
-    def current_user(self, request=None) -> User:
+        return request.headers.get('Authorization', None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
         """
-        returns None - request
+            Look current user
+
+            Args:
+                request: Look the reques user
+
+            Return:
+                The user
         """
-        return None
+        return request
